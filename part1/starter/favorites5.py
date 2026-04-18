14d357af72392591ad89781a44d40e343d4cbecd
Bjetyl Xhemail <119294094+bx29361@users.noreply.github.com>
2026-04-19 00:07:46 +0200

    tuana

--- part1/starter/favorites5.py
+++ part1/starter/favorites5.py
@@ -19,8 +19,15 @@ with open("favorites.csv", "r") as file:
 
     for row in reader:
         favorite = row["language"]
+        
         # TODO: If favorite is already in counts, increment counts[favorite] by 1
         # TODO: Otherwise, set counts[favorite] = 1
+        if favorite in counts:
+            counts[favorite] += 1
+        else:
+            counts[favorite] = 1
 
     # TODO: Print each key-value pair in counts
     #       Format: "Python: 196"
+    for language, count in counts.items():
+        print(f"{language}: {count}")
